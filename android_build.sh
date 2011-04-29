51c31711a3689c66b7eff04c0d2c1b4a5fc5010c
Jahrome <jahrome11@gmail.com>
2011-04-29 17:46:31 +0200

    Update build script

--- android_build.sh
+++ android_build.sh
@@ -1,3 +1,19 @@
+export NDK=/home/jer/Projet_android/android-ndk-r5
+export TOOLCHAIN=/home/jer/standalone-toolchain
+export AOSP=/home/jer/cm7
+export PRODUCT=vision
+export PATH=$TOOLCHAIN/bin/:$PATH
+
+export CC=arm-linux-androideabi-gcc
+export CFLAGS="-march=armv7-a -mfloat-abi=softfp"
+export CPPFLAGS=$CFLAGS
+export LDFLAGS="-Wl,--fix-cortex-a8 -lsupc++ -L$AOSP/out/target/product/$PRODUCT/system/lib"
+export LIBS="$TOOLCHAIN/arm-linux-androideabi/lib/libstdc++.a"
+
+rm -rf $TOOLCHAIN
+$NDK/build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=$TOOLCHAIN
+
 # Configure using host parameters (cross compilation not supported)
 ./configure
 make -f Makefile.android
+arm-linux-androideabi-strip cntlm
